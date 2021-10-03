5d55843e1799ae488dda033c7a54231faca7f0a0
千叶 <134857853@qq.com>
2021-10-03 17:47:34 +0800

    10月1日,Mybatis学习完毕(修订)

---
+++ Mybatis08/src/test/java/com/zhoujk/mapper/UserMapperTest.java
@@ -0,0 +1,40 @@
+package com.zhoujk.mapper;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA
+ *
+ * @Author: ZhouJiankang
+ * @Date: 2021/10/3  11:17
+ * @Description:
+ */
+public class UserMapperTest
+{
+
+    @Before
+    public void setUp() throws Exception
+    {
+    }
+
+    @After
+    public void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    public void insert()
+    {
+    }
+
+    @Test
+    public void insertSelective()
+    {
+
+
+    }
+}
